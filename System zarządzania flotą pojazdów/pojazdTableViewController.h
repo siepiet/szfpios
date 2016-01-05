0763b3323337b2a3cf3752165332a2dbdb763f8f
user <user@admins-mac-mini.student.wat.edu.pl>
2016-01-06 00:34:23 +0100

    Final version

---
+++ System zarządzania flotą pojazdów/pojazdTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  pojazdTableViewController.h
+//  System zarządzania flotą pojazdów
+//
+//  Created by user on 05.01.2016.
+//  Copyright © 2016 Mateusz Siepietowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface pojazdTableViewController : UITableViewController
+
+@end
